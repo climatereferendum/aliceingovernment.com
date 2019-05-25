ea745c49b32cb90566ba8e434dfe9925c66233ab
elf Pavlik <elf-pavlik@hackers4peace.net>
2019-05-25 14:19:33 -0500

    initial working connection to backend

--- index.js
+++ index.js
@@ -1,31 +1,51 @@
 import { html, render } from 'lit-html'
 import { installRouter } from './node_modules/pwa-helpers/router.js'
 import csv from 'neat-csv'
-import { flag } from 'country-emoji'
+import { flag, name as countryName, countries } from 'country-emoji'
+
+const { fetch, localStorage, FormData } = window
 
 const DOC_URL = 'https://docs.google.com/spreadsheets/d/1WNDWjJOGeVbOsYaWy3udBnRxFIknO5NpYwToVhH2nGE/gviz/tq?tqx=out:csv'
+const SERVICE_URL = 'https://elf.pagekite.me'
 const PREVIEW_VOTES_COUNT = 5
 let solutions
 let votes, votesCount
-let myVotes = []
+let myVote
+let active, slug
+let selectedSolutions = []
+let authProviders
+
 const form = document.querySelector('#vote-form')
+renderCountriesDropdown()
 form.addEventListener('submit', (event) => {
   event.preventDefault()
   event.stopPropagation()
-  const data = new FormData (form)
-  for (const key of data.keys()) { console.log(key, data.get(key)) }
+  handleSubmit()
   return false
 })
 
-function updateMyVotes (event) {
-  myVotes.push(event.target.value)
+function handleSubmit () {
+  const data = new FormData(form)
+  const draft = {}
+  for (const key of data.keys()) { (draft[key] = data.get(key)) }
+  draft.solution = [...selectedSolutions]
+  localStorage.setItem('data', JSON.stringify(draft))
+  window.location = `${SERVICE_URL}${authProviders[draft.auth]}`
+}
+
+function updateSelectedSolutions (event) {
+  if (event.target.checked) {
+    selectedSolutions.push(event.target.value)
+  } else {
+    selectedSolutions = selectedSolutions.filter(s => s !== event.target.value)
+  }
   const solutionElements = document.querySelectorAll('#solutions .project-box')
   const nav = document.querySelector('.sticky-nav')
   const footer = document.querySelector('footer')
-  if (myVotes.length === 3) {
+  if (selectedSolutions.length === 3) {
     // hide other solutions
     for (const element of solutionElements) {
-      if (!myVotes.includes(element.dataset.rank)) {
+      if (!selectedSolutions.includes(element.dataset.rank)) {
         element.classList.add('inactive')
       }
     }
@@ -46,18 +66,62 @@ function updateMyVotes (event) {
   }
 }
 
+function showVote (vote) {
+  window.history.pushState({}, '', '/info')
+  handleRouting(window.location)
+}
+
 ;(async () => {
-  const solutionsResponse = await window.fetch(DOC_URL + '&sheet=solutions')
+  const solutionsResponse = await fetch(DOC_URL + '&sheet=solutions')
   const solutionsCsv = await solutionsResponse.text()
   solutions = await csv(solutionsCsv)
   renderSolutions(solutions)
   installRouter(handleRouting)
+  const serviceResponse = await fetch(SERVICE_URL, { credentials: 'include' })
+  const serviceData = await serviceResponse.json()
+  authProviders = serviceData.authProviders
+  myVote = serviceData.vote
+  console.log('vote:', myVote)
+  let savedData = localStorage.getItem('data')
+  if (savedData) {
+    savedData = JSON.parse(savedData)
+  }
+  console.log('savedData:', savedData)
+  if (!myVote) {
+    // not authenticated
+    // TODO: if savedData should continue with voting
+  } else if (myVote.solutions) {
+    // already voted
+    showVote()
+  } else if (savedData) {
+    // vote ready to submit
+    myVote.solutions = selectedSolutions
+    Object.assign(myVote, savedData)
+    const castedVoteResponse = await fetch(myVote.id, {
+      method: 'PUT',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(myVote)
+    })
+    if (castedVoteResponse.ok) {
+      console.log('VOTE SUBMISSION SUCCEEDED')
+      // draft
+      localStorage.removeItem('data')
+      // re fetch votes
+      ;[votes, votesCount] = await fetchVotes()
+      showVote(myVote)
+    } else {
+      console.log('VOTE SUBMISSION FAILED')
+    }
+  } else {
+    console.log('AUTHENTICATED BUT NO SAVED DATA')
+  }
 })()
 
 const pages = document.querySelectorAll('.page')
 const nav = {
   solutions: document.querySelector('#nav-solutions'),
-  votes: document.querySelector('#nav-votes'),
+  voters: document.querySelector('#nav-voters'),
   info: document.querySelector('#nav-info')
 }
 
@@ -72,36 +136,41 @@ async function handleRouting (location, event) {
     nav[n].classList.remove('active')
     nav[n].classList.remove('active-prev')
   }
-  const active = location.pathname.split('/')[1]
-  const slug = location.pathname.split('/')[2]
+  active = location.pathname.split('/')[1]
+  slug = location.pathname.split('/')[2]
   let linkedHeader = true
   if (active === '') {
     document.querySelector('#home').classList.remove('inactive')
     linkedHeader = false
   }
   if (active === 'solutions' && !slug) {
+    nav['voters'].classList.add('active-prev')
     nav['solutions'].classList.add('active')
     document.querySelector('#solutions').classList.remove('inactive')
   }
-  if (active === 'votes' && !slug) {
-    nav['solutions'].classList.add('active-prev')
-    nav['votes'].classList.add('active')
+  if (active === 'voters' && !slug) {
+    nav['voters'].classList.add('active')
     if (!votes) [votes, votesCount] = await fetchVotes()
+    if (active !== 'voters') return // check again if route didn't change
     renderVotes(votes)
-    document.querySelector('#votes').classList.remove('inactive')
+    document.querySelector('#voters').classList.remove('inactive')
   }
   if (active === 'info') {
+    nav['voters'].classList.add('active-prev')
     nav['solutions'].classList.add('active-prev')
-    nav['votes'].classList.add('active-prev')
     nav['info'].classList.add('active')
+    if (myVote && myVote.nationality) {
+      if (!votes) [votes, votesCount] = await fetchVotes()
+      render(countryShortTemplate(myVote.nationality), document.querySelector('#my-vote'))
+    }
     document.querySelector('#info').classList.remove('inactive')
   }
-  if (active === 'votes' && slug) {
-    nav['solutions'].classList.add('active-prev')
-    nav['votes'].classList.add('active')
+  if (active === 'voters' && slug) {
+    nav['voters'].classList.add('active')
     if (!votes) [votes, votesCount] = await fetchVotes()
     const country = Object.keys(votes).find(country => country.toLowerCase() === slug)
     const countryVotes = votes[country]
+    if (active !== 'voters') return // check again if route didn't change
     renderCountry(country, countryVotes)
     document.querySelector('#country').classList.remove('inactive')
   }
@@ -109,14 +178,13 @@ async function handleRouting (location, event) {
 }
 
 async function fetchVotes () {
-  const votesResponse = await window.fetch(DOC_URL + '&sheet=votes')
-  const votesCsv = await votesResponse.text()
-  const votes = await csv(votesCsv)
+  const votesResponse = await window.fetch(SERVICE_URL + '/votes')
+  const votes = await votesResponse.json()
   const reduced = votes.reduce((acc, vote) => {
-    if (!acc[vote.country]) {
-      acc[vote.country] = []
+    if (!acc[vote.nationality]) {
+      acc[vote.nationality] = []
     }
-    acc[vote['country']].push(vote)
+    acc[vote['nationality']].push(vote)
     return acc
   }, {})
   // add index per country
@@ -127,6 +195,17 @@ async function fetchVotes () {
     reduced[country].reverse()
   }
   return [reduced, votes.length]
+}
+
+function renderCountriesDropdown () {
+  const template = html`
+    <p><select name="nationality">
+      ${Object.keys(countries).map(code => html`
+        <option value="${code.toLowerCase()}">${countries[code]} ${flag(code)}</option>
+      `)}
+    </select></p>
+  `
+  render(template, document.querySelector('#nationality'))
 }
 
 function renderHeader (linked = true) {
@@ -147,18 +226,18 @@ function renderHeader (linked = true) {
 function solutionTemplate (solution) {
   return html`
     <div class="project-box col-xs-6" data-rank=${solution.rank}>
-        <a href="${solution.link}" target="drawdown">
-        <h4>Solution #${solution.rank}</h4>
-        <h3>${solution.name}</h3></a>
-        <label class="container">
-          <input
-            type="checkbox"
-            name="solution-${solution.rank}"
-            value="${solution.rank}"
-            @change="${updateMyVotes}"
-            >
-          <span class="checkmark"></span>
-        </label> 
+      <a href="${solution.link}" target="drawdown">
+      <h4>Solution #${solution.rank}</h4>
+      <h3>${solution.name}</h3></a>
+      <label class="container">
+        <input
+          type="checkbox"
+          name="solution-${solution.rank}"
+          value="${solution.rank}"
+          @change="${updateSelectedSolutions}"
+          >
+        <span class="checkmark"></span>
+      </label> 
     </div>
   `
 }
@@ -190,29 +269,31 @@ function loadMoreLink (country, countryVotes) {
   }
 }
 
+function countryShortTemplate (country) {
+  return html`
+    <div class="vertical-line"></div>
+    <div class="project-box votes">
+      <h2>
+        ${flag(country)}
+        ${countryName(country)}
+      </h2>
+      <span>${votes[country].length} VOTES</span>
+    </div>
+    <div class="project-box solution content">
+        <ul>
+          ${votes[country].slice(PREVIEW_VOTES_COUNT * -1).map(voteTemplate)}
+        </ul>
+        ${loadMoreLink(country, votes[country])}
+    </div>
+  `
+}
+
 function renderVotes (votes) {
   const orderedCountries = Object.keys(votes).sort((first, second) => {
     return votes[second].length - votes[first].length
   })
   const listTemplate = html`${
-    orderedCountries.map(country => {
-      return html`
-        <div class="vertical-line"></div>
-        <div class="project-box votes">
-          <h2>
-            ${flag(country)}
-            ${country}
-          </h2>
-          <span>${votes[country].length} VOTES</span>
-        </div>
-        <div class="project-box solution content">
-            <ul>
-              ${votes[country].slice(PREVIEW_VOTES_COUNT * -1).map(voteTemplate)}
-            </ul>
-            ${loadMoreLink(country, votes[country])}
-        </div>
-      `
-    })
+    orderedCountries.map(countryShortTemplate)
   }`
   const pageTemplate = html`
     <div class="flex-wrap">
@@ -225,7 +306,7 @@ function renderVotes (votes) {
       ${listTemplate}
     </div>
   `
-  render(pageTemplate, document.querySelector('#votes'))
+  render(pageTemplate, document.querySelector('#voters'))
 }
 
 function voteTemplate (vote) {
@@ -233,7 +314,7 @@ function voteTemplate (vote) {
   <li>
     <em class="index">${vote.index}</em>
     <strong>${vote.name}</strong>
-    <em>${vote.organization}</em>
+    <em>${vote.description}</em>
   </li>
   `
 }
@@ -243,7 +324,7 @@ function renderCountry (country, countryVotes) {
     <div class="project-box votes">
       <h2>
         ${flag(country)}
-        ${country}
+        ${countryName(country)}
       </h2>
       <span>${votes[country].length} VOTES</span>
     </div>
