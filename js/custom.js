8c65086de3ec530ca2aedfec3f25b7d503d9ead4
elf Pavlik <elf-pavlik@hackers4peace.net>
2019-05-27 18:06:57 -0500

    custom.js uses https://standardjs.com/

--- js/custom.js
+++ js/custom.js
@@ -1,86 +1,89 @@
-//FAQ info accordion
-var acc = document.getElementsByClassName("accordion");
-var i;
+// FAQ info accordion
+var acc = document.getElementsByClassName('accordion')
+var i
 
 for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
+  acc[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+    var panel = this.nextElementSibling
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null
     } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
+      panel.style.maxHeight = panel.scrollHeight + 'px'
+    }
+  })
 }
 
 /* Multistep Form JS */
 
-var currentTab = 0; // Current tab is set to be the first tab (0)
-showTab(currentTab); // Display the current tab
+var currentTab = 0 // Current tab is set to be the first tab (0)
+showTab(currentTab) // Display the current tab
 
-function showTab(n) {
-    // This function will display the specified tab of the form ...
-    var x = document.getElementsByClassName("tab");
-    x[n].style.display = "block";
-    // ... and fix the Previous/Next buttons:
-    if (n == 0) {
-    document.getElementById("prevBtn").style.display = "none";
-    } else {
-    document.getElementById("prevBtn").style.display = "inline";
-    }
-    if (n == (x.length - 1)) {
-    document.getElementById("nextBtn").classList.add('inactive');
-    } else {
-    document.getElementById("nextBtn").classList.remove('inactive');
-    }
-    // ... and run a function that displays the correct step indicator:
-    fixStepIndicator(n)
+function showTab (n) {
+  // This function will display the specified tab of the form ...
+  var x = document.getElementsByClassName('tab')
+  x[n].style.display = 'block'
+  // ... and fix the Previous/Next buttons:
+  if (n === 0) {
+    document.getElementById('prevBtn').style.display = 'none'
+  } else {
+    document.getElementById('prevBtn').style.display = 'inline'
+  }
+  if (n === x.length - 1) {
+    document.getElementById('nextBtn').classList.add('inactive')
+  } else {
+    document.getElementById('nextBtn').classList.remove('inactive')
+  }
+  // ... and run a function that displays the correct step indicator:
+  fixStepIndicator(n)
 }
 
-function nextPrev(n) {
-    // This function will figure out which tab to display
-    var x = document.getElementsByClassName("tab");
-    // Exit the function if any field in the current tab is invalid:
-    if (n == 1 && !validateForm()) return false;
-    // Hide the current tab:
-    x[currentTab].style.display = "none";
-    // Increase or decrease the current tab by 1:
-    currentTab = currentTab + n;
-    // if you have reached the end of the form... :
-    // Otherwise, display the correct tab:
-    showTab(currentTab);
+function nextPrev (n) {
+  // This function will figure out which tab to display
+  var x = document.getElementsByClassName('tab')
+  // Exit the function if any field in the current tab is invalid:
+  if (n === 1 && !validateForm()) return false
+  // Hide the current tab:
+  x[currentTab].style.display = 'none'
+  // Increase or decrease the current tab by 1:
+  currentTab = currentTab + n
+  // if you have reached the end of the form... :
+  // Otherwise, display the correct tab:
+  showTab(currentTab)
 }
 
-function validateForm() {
-    // This function deals with validation of the form fields
-    var x, y, i, valid = true;
-    x = document.getElementsByClassName("tab");
-    y = x[currentTab].getElementsByTagName("input");
-    // A loop that checks every input field in the current tab:
-    for (i = 0; i < y.length; i++) {
+function validateForm () {
+  // This function deals with validation of the form fields
+  var x
+  var y
+  var i
+  var valid = true
+  x = document.getElementsByClassName('tab')
+  y = x[currentTab].getElementsByTagName('input')
+  // A loop that checks every input field in the current tab:
+  for (i = 0; i < y.length; i++) {
     // If a field is empty...
-    if (y[i].value == "") {
-        // add an "invalid" class to the field:
-        y[i].className += " invalid";
-        // and set the current valid status to false:
-        valid = false;
+    if (y[i].value === '') {
+      // add an "invalid" class to the field:
+      y[i].className += ' invalid'
+      // and set the current valid status to false:
+      valid = false
     }
-    }
-    // If the valid status is true, mark the step as finished and valid:
-    if (valid) {
-    document.getElementsByClassName("step")[currentTab].className += " finish";
-    }
-    return valid; // return the valid status
+  }
+  // If the valid status is true, mark the step as finished and valid:
+  if (valid) {
+    document.getElementsByClassName('step')[currentTab].className += ' finish'
+  }
+  return valid // return the valid status
 }
 
-function fixStepIndicator(n) {
-    // This function removes the "active" class of all steps...
-    var i, x = document.getElementsByClassName("step");
-    for (i = 0; i < x.length; i++) {
-    x[i].className = x[i].className.replace(" active", "");
-    }
-    //... and adds the "active" class to the current step:
-    x[n].className += " active";
+function fixStepIndicator (n) {
+  // This function removes the "active" class of all steps...
+  var i
+  var x = document.getElementsByClassName('step')
+  for (i = 0; i < x.length; i++) {
+    x[i].className = x[i].className.replace(' active', '')
+  }
+  // ... and adds the "active" class to the current step:
+  x[n].className += ' active'
 }
-
